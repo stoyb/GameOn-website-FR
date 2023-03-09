ac92e54a9588dc230041eb7a73cdc07cc3c5812f
Sonia <stoyb@github.com>
2023-03-09 21:20:36 +0100

    bugs

--- modal.js
+++ modal.js
@@ -11,8 +11,13 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const closeModalBtn = document.querySelector(".close");
+const closeModalBtn = document.querySelectorAll(".close");
 const contentModal = document.querySelector(".content"); 
+const btnSubmit = document.querySelector(".btn-submit");
+const modalbgValidate = document.querySelector(".bground-valid");
+const closeBtn = document.querySelector(".close-btn");
+const form = document.querySelector(".myForm");
+
 
 
 // launch modal event
@@ -29,17 +34,22 @@ function closeModal() {
 }
 
 // Close modal with the button x
-closeModalBtn.addEventListener("click", closeModal);
+closeModalBtn.forEach((closebtn) => closebtn.addEventListener("click", closeModal));
 // Close modal with the shadow without propagrate the click event to the modal content
 modalbg.addEventListener('click', closeModal);
 contentModal.addEventListener('click', (e) => {
   e.stopPropagation();
 });
 
+
+function modalValidate() {
+  modalbgValidate.style.display = "block";
+  modalbg.style.display = "none";
+}
+
 function validate() {
   let formFirst = document.forms["reserve"]["first"].value;
   let formLast = document.forms["reserve"]["last"].value;
-  let radio = document.forms["reserve"]["location"].value;
   let formDate = document.forms["reserve"]["birthdate"].value; 
   let checkbox = document.getElementById('checkbox1');
   
@@ -47,24 +57,42 @@ function validate() {
     alert('Veuillez entrer 2 caractères ou plus pour le champ du prénom.');
     return false;
   }
-  if (formLast.length < 2 ) {
+  else if (formLast.length < 2 ) {
     alert('Veuillez entrer 2 caractères ou plus pour le champ du nom.');
     return false;
   }
-  if (!radio.checked ) {
-    alert('Vous devez choisir une option.');
-    return false;
-  }
-  if (formDate == "" ) {
+  else if (formDate == "" ) {
     alert("Vous devez entrer votre date de naissance.")
     return false;
   }
-  if (!checkbox.checked) {
+  else if (!checkbox.checked) {
     alert('Vous devez vérifier que vous acceptez les termes et conditions.');
     return false;
+  } 
+  else {
+  return modalValidate();
+  }
+}
+
+  function preventSubmit(e) {
+    e.preventDefault();
+    btnSubmit.addEventListener('click', modalValidate);
+  }
+  form.addEventListener('submit', preventSubmit);
+  
+  function closeModalValidate() {
+    modalbgValidate.style.display = "none";
+    window.location.reload();
   }
 
-  }
+  const closeModalContentValidate = document.querySelector('.content-valid');
+  const closeModalBtnValidate = document.querySelector('.close-valid');
+  closeModalBtnValidate.addEventListener('click', closeModalValidate);
+  modalbgValidate.addEventListener('click', closeModalValidate);
+  closeModalContentValidate.addEventListener('click', (e) =>
+    e.stopPropagation());
+  closeBtn.addEventListener('click', closeModalValidate);
+
 
 
 
