f44178e333f8f9dcc996809eb3809d28402ebca4
Sonia <stoyb@github.com>
2023-03-10 16:13:33 +0100

    wipe code

--- modal.js
+++ modal.js
@@ -1,3 +1,15 @@
+              /* DOM Elements */
+              
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const closeModalBtn = document.querySelectorAll(".close");
+const contentModal = document.querySelector(".content"); 
+const btnSubmit = document.querySelector(".btn-submit");
+const form = document.querySelector(".myForm");
+
+                /* Functions */ 
+
+// Hamburger navbar mobile device            
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -6,22 +18,6 @@ function editNav() {
     x.className = "topnav";
   }
 }
-
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const closeModalBtn = document.querySelectorAll(".close");
-const contentModal = document.querySelector(".content"); 
-const btnSubmit = document.querySelector(".btn-submit");
-const modalbgValidate = document.querySelector(".bground-valid");
-const closeBtn = document.querySelector(".close-btn");
-const form = document.querySelector(".myForm");
-
-
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
@@ -33,8 +29,14 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+              /* Call functions */
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
 // Close modal with the button x
 closeModalBtn.forEach((closebtn) => closebtn.addEventListener("click", closeModal));
+
 // Close modal with the shadow without propagrate the click event to the modal content
 modalbg.addEventListener('click', closeModal);
 contentModal.addEventListener('click', (e) => {
@@ -42,11 +44,27 @@ contentModal.addEventListener('click', (e) => {
 });
 
 
+                        /* Validation  data */
+
+
+
+                  /* DOM Elements Validation data */
+
+const modalbgValidate = document.querySelector(".bground-validate");
+const closeModalContentValidate = document.querySelector('.content-validate');
+const closeModalBtnValidate = document.querySelector('.close-validate');
+const closeBtnValidate = document.querySelector(".close-btn-validate");
+
+
+                  /* Functions Validation data */ 
+
+// The validate modal appears and the fomular modal disappears                  
 function modalValidate() {
   modalbgValidate.style.display = "block";
   modalbg.style.display = "none";
 }
 
+// Validate the submit and sends error messages 
 function validate() {
   let formFirst = document.forms["reserve"]["first"].value;
   let formLast = document.forms["reserve"]["last"].value;
@@ -74,24 +92,33 @@ function validate() {
   }
 }
 
-  function preventSubmit(e) {
-    e.preventDefault();
-    btnSubmit.addEventListener('click', modalValidate);
-  }
-  form.addEventListener('submit', preventSubmit);
-  
-  function closeModalValidate() {
-    modalbgValidate.style.display = "none";
-    window.location.reload();
-  }
+// Prevent the submit function for the button linked to the formular
+function preventSubmit(e) {
+  e.preventDefault();
+  btnSubmit.addEventListener('click', modalValidate);
+}
 
-  const closeModalContentValidate = document.querySelector('.content-valid');
-  const closeModalBtnValidate = document.querySelector('.close-valid');
-  closeModalBtnValidate.addEventListener('click', closeModalValidate);
-  modalbgValidate.addEventListener('click', closeModalValidate);
-  closeModalContentValidate.addEventListener('click', (e) =>
-    e.stopPropagation());
-  closeBtn.addEventListener('click', closeModalValidate);
+// Close the validate modal and refresh the page 
+function closeModalValidate() {
+  modalbgValidate.style.display = "none";
+  window.location.reload();
+}
+
+                  /* Call functions Validate */
+
+// Prevent formular's submit
+form.addEventListener('submit', preventSubmit); 
+
+// Close the validate modal with the button x
+closeModalBtnValidate.addEventListener('click', closeModalValidate);
+
+// Close the validate modal with the shadow
+modalbgValidate.addEventListener('click', closeModalValidate);
+closeModalContentValidate.addEventListener('click', (e) =>
+  e.stopPropagation());
+
+// Close the validate modal with the button "Close"
+closeBtnValidate.addEventListener('click', closeModalValidate);
 
 
 
